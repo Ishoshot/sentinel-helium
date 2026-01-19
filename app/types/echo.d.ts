a8c0944764115bffb1fea2c120fa812a7cf92060
Oluwatobi Ishola <ishoshot@gmail.com>
2026-01-19 15:52:43 +0100

    chore(deps): update dependencies and add Laravel Echo

---
+++ app/types/echo.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Laravel Echo type declarations for Nuxt
+ */
+
+import type Echo from 'laravel-echo';
+
+declare module '#app' {
+  interface NuxtApp {
+    $echo: Echo<'reverb'>;
+  }
+}
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $echo: Echo<'reverb'>;
+  }
+}
+
+export {};
