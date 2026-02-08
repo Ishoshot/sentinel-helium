8504c29d0b84b384427c3afadc9b2208bccededa
Oluwatobi Ishola <ishoshot@gmail.com>
2026-02-08 18:36:18 +0100

    refactor(assets): update favicon assets and remove web manifest
    Update all favicon files (ico, 16x16, 32x32, apple-touch-icon). Remove Android Chrome icons, site.webmanifest, and manifest link from nuxt config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,6 @@ export default defineNuxtConfig({
           sizes: "180x180",
           href: "/apple-touch-icon.png",
         },
-        { rel: "manifest", href: "/site.webmanifest" },
       ],
     },
   },
